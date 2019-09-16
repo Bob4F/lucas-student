40fd5f32f6d2aff3d45901539309fb581031b213
13715166404@163.com <13715166404@163.com>
2019-09-17 00:11:53 +0800

    up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # lucas-student
-Spring Boot 集成 Redis
+Spring Boot 集成 Redis和 jedis
 主要是想完善下 Redis 的api的应用
 顺便学习相关的一些知识
 
---
+++ redis-distributed/src/main/java/github/lucas/redis/distributed/jedis/RedisContent.java
@@ -0,0 +1,41 @@
+package github.lucas.redis.distributed.jedis;
+
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @author Lucas
+ * @ClassName RedisTemplate
+ * @Data 2019-09-16
+ * @Version 1.0
+ */
+@Component
+public class RedisContent {
+
+    public JedisPool jedisPool =null;
+
+    public Jedis jedis = null;
+
+    /**
+     * 无参：初始化参数
+     */
+    public RedisContent(){
+        try {
+            JedisPoolConfig config = new JedisPoolConfig();
+            // 允许最多创建多少个redis操作实例
+            config.setMaxTotal(1024);
+            // 允许空闲时 连接池中最多有多少个redis操作实例
+            config.setMaxIdle(20);
+            // 一个redis最大可用时间，过时销毁实例
+            config.setMaxWaitMillis(10000*60);
+
+            config.setTestOnBorrow(true);
+            jedisPool = new JedisPool(config, "47.92.86.79", 6379, 10000, "sunfounder");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
