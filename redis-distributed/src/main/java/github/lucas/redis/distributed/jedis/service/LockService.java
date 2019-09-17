f54c6878095d8a567a7efad8321e8b25f2023ab7
13715166404@163.com <13715166404@163.com>
2019-09-17 15:17:10 +0800

    up distributed lock file

--- redis-distributed/src/main/java/github/lucas/redis/distributed/jedis/service/LockService.java
+++ redis-distributed/src/main/java/github/lucas/redis/distributed/jedis/service/LockService.java
@@ -4,6 +4,9 @@ import github.lucas.redis.distributed.jedis.RedisContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.Collections;
 
 /**
  * @author Lucas
@@ -19,22 +22,45 @@ public class LockService {
 
     private Jedis jedis=null;
 
+    private static final String LOCK_MSG = "OK";
+
+    private static final Long UNLOCK_MSG = 1L;
+
     private static final String SET_IF_NOT_EXIST = "NX";
 
     private static final String SET_WITH_EXPIRE_TIME = "PX";
 
     /**
-     * 加锁
+     * 加锁 一分钟内过期
      * @param key 锁的key
      * @param request 要设置的值
      * @return
      */
     public boolean tryLock(String key,String request){
         jedis = redisContent.jedisPool.getResource();
-        String result = jedis.set("LOCK"+key,request,SET_IF_NOT_EXIST,SET_WITH_EXPIRE_TIME,10*60);
+        String result = jedis.set("lock-"+key,request,SET_IF_NOT_EXIST,SET_WITH_EXPIRE_TIME,10*60000);
         System.out.println(result);
         jedis.close();
         return false;
+    }
+
+
+    public boolean unlock(String key,String request){
+            //lua script
+            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+            Object result = null ;
+            jedis = redisContent.jedisPool.getResource();
+            if (jedis !=null){
+                result = (jedis).eval(script, Collections.singletonList("lock-" + key), Collections.singletonList(request));
+            }else {
+                return false ;
+            }
+            System.out.println(result);
+            if (UNLOCK_MSG.equals(result)){
+                return true ;
+            }
+            System.out.println("解锁失败");
+            return false;
     }
 
 }
--- redis-distributed/src/test/java/github/lucas/redis/distributed/RedisDistributedLockApplicationTests.java
+++ redis-distributed/src/test/java/github/lucas/redis/distributed/RedisDistributedLockApplicationTests.java
@@ -16,10 +16,22 @@ public class RedisDistributedLockApplicationTests {
 
 
 	@Test
-	public void tt(){
+	public void redisLock() throws InterruptedException {
 		String key = "spring-boot";
-		String val = "start redis";
+		String val = "start redis2333333";
 		lock.tryLock(key,val);
+		System.out.println("上锁成功！");
+		System.out.println("尝试解锁");
+		StringBuffer stringBuffer =new StringBuffer("start redis23");
+		while(true){
+			boolean flag = lock.unlock(key,stringBuffer.toString());
+			if (flag){
+				break;
+			}
+			Thread.sleep(1000);
+			stringBuffer = stringBuffer.append("3");
+		}
+		System.out.println("解锁成功");
 	}
 
 }
