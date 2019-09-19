3709ed89f0e48f59d001e4d8f2fff6f74d1bcebc
13715166404@163.com <13715166404@163.com>
2019-09-19 22:02:54 +0800

    up mac

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # lucas-student
-Spring Boot 集成 Redis和 jedis
-主要是想完善下 Redis 的api的应用
-顺便学习相关的一些知识
-
+用java实现的一些功能
 ## project list
 +  redis-distributed：  
-     + redis(Jedis框架) 实现分布式锁
+     Spring Boot 集成 jedis
+     主要是想完善下 Redis 的api的应用
+     顺便学习相关的一些知识，实现了分布式锁
      
--- redis-distributed/src/main/java/github/lucas/redis/distributed/jedis/service/LockService.java
+++ redis-distributed/src/main/java/github/lucas/redis/distributed/jedis/service/LockService.java
@@ -22,7 +22,7 @@ public class LockService {
 
     private Jedis jedis=null;
 
-    private static final String LOCK_MSG = "OK";
+    private static final String LOCK_RESULT = "OK";
 
     private static final Long UNLOCK_MSG = 1L;
 
@@ -31,18 +31,38 @@ public class LockService {
     private static final String SET_WITH_EXPIRE_TIME = "PX";
 
     /**
-     * 加锁 一分钟内过期
+     * 加锁 5s内过期
      * @param key 锁的key
      * @param request 要设置的值
      * @return
      */
     public boolean tryLock(String key,String request){
         jedis = redisContent.jedisPool.getResource();
-        String result = jedis.set("lock-"+key,request,SET_IF_NOT_EXIST,SET_WITH_EXPIRE_TIME,10*60000);
+        String result = jedis.set("lock-"+key,request,SET_IF_NOT_EXIST,SET_WITH_EXPIRE_TIME,5000);
         System.out.println(result);
         jedis.close();
         return false;
     }
+
+    /**
+     * 阻塞锁 只锁5s
+     * @param key
+     * @param val
+     * @return
+     * @throws InterruptedException
+     */
+    public void lock(String key,String val) throws InterruptedException {
+        jedis = redisContent.jedisPool.getResource();
+        for (;;){
+            String result = jedis.set(key,val,"NX","PX",5000);
+            if (LOCK_RESULT.equals(result)){
+                break;
+            }
+            // 防止消耗CPU
+            Thread.sleep(10000);
+        }
+    }
+
 
 
     public boolean unlock(String key,String request){
